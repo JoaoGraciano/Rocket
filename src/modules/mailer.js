1a97aef705179daf2d3b070b9e7ef5eb7d425c9d
JoaoGraciano <joaovitor.graciano20@gmail.com>
2022-04-18 17:58:37 -0300

    atualizacao

--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -8,4 +8,10 @@ const transport = nodemailer.createTransport({
     auth: { user, pass }
   });
 
+  transport.use('compile', hbs({
+      viewEngine: 'handlebars',
+      viewPath: path.resolve('./src/resources/mail/')
+      extName: '.html',
+    }));
+
 module.exports = transport;
