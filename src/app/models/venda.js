bf669d3c7cda559a3e22b72164a67eae9bfdac50
JoaoGraciano <joaovitor.graciano20@gmail.com>
2022-05-13 16:55:19 -0300

    at13.2

--- src/app/models/venda.js
+++ src/app/models/venda.js
@@ -19,7 +19,7 @@ const UserSchema = new mongoose.Schema({
         type:String,
         require: true,
     },
-    clickedRows: {
+    cursos: {
         type:String,
         require: true,
     },
